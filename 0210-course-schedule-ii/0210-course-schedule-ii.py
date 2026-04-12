0665f708924005c87e08c401b4e9fcb4e2aa5fe1
V. Manusri <velpula.manusri06@gmail.com>
2026-04-12 18:47:41 +0530

    Time: 0 ms (100%), Space: 20.5 MB (62.83%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -8,6 +8,28 @@ class Solution:
         for u, v in prerequisites:
             adj[v].append(u)
             indegree[u] += 1
+
+        q = deque()
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                q.append(i)
+        topo = []
+        while q:
+            node = q.popleft()
+            topo.append(node)
+
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+        return topo if len(topo) == numCourses else [] 
+
+        """ indegree = [0]*numCourses
+        adj = [[] for _ in range(numCourses)]
+
+        for u, v in prerequisites:
+            adj[v].append(u)
+            indegree[u] += 1
         
         q = deque()
         for i in range(len(indegree)):
@@ -27,3 +49,4 @@ class Solution:
             return []
         
         return ans
+ """
