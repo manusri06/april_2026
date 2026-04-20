466409fa3d66aff77606f7e5755dd32c8081543b
V. Manusri <velpula.manusri06@gmail.com>
2026-04-20 23:05:38 +0530

    Time: 7 ms (28.61%), Space: 20.4 MB (77.63%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.py
+++ 0210-course-schedule-ii/0210-course-schedule-ii.py
@@ -1,7 +1,5 @@
-from collections import deque
 class Solution:
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
-
         indegree = [0]*numCourses
         adj = [[] for _ in range(numCourses)]
 
@@ -23,30 +21,3 @@ class Solution:
                 if indegree[nei] == 0:
                     q.append(nei)
         return topo if len(topo) == numCourses else [] 
-
-        """ indegree = [0]*numCourses
-        adj = [[] for _ in range(numCourses)]
-
-        for u, v in prerequisites:
-            adj[v].append(u)
-            indegree[u] += 1
-        
-        q = deque()
-        for i in range(len(indegree)):
-            if indegree[i] == 0:
-                q.append(i)
-
-        ans = []
-        while q:
-            node = q.popleft()
-            ans.append(node)
-            for nei in adj[node]:
-                indegree[nei] -= 1
-                if indegree[nei] == 0:
-                    q.append(nei)
-
-        if len(ans) != numCourses:
-            return []
-        
-        return ans
- """
